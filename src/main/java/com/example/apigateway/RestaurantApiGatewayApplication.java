904ca717347d19b555a85419b9f1385d317fd6e7
Mahadi Hasan <md.mahadihasan300@gmail.com>
2025-03-16 20:52:46 +0600

    api gateway init

--- src/main/java/com/example/apigateway/RestaurantApiGatewayApplication.java
+++ src/main/java/com/example/apigateway/RestaurantApiGatewayApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PathokApiGatewayApplication {
+public class RestaurantApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PathokApiGatewayApplication.class, args);
+		SpringApplication.run(RestaurantApiGatewayApplication.class, args);
 	}
 
 }
